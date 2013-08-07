9e0afb90af7456034ad00a3ef4d121faa50633b6
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 09:58:09 -0500

    solution to lucky ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
 
+  $("form").submit(function(event){
+    event.preventDefault();
+    console.log("Hey we're here");
+
+    $.post("/rolls", function(server_response){
+      console.log(server_response);
+      $('#die').html(server_response);
+    });
+  });
   // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
+  //   1- intercept the form submission event using jQuery - x
+  //   2- prevent the default action for that event from happening -x
   //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
